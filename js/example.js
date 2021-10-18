a9a4ec6c6d0c3b979d8156f4517bcd208a79246e
Jeancahu <jeancahu@gmail.com>
2021-10-18 14:02:43 -0600

    chg: enable popup content

--- js/example.js
+++ js/example.js
@@ -1,10 +1,30 @@
-import {streetElementGroup} from "../lib/streetelement.js";
+import { streetElementGroup } from "../lib/streetelement.js";
 
 // Create a new StreetElementGroup
 console.log("Create the object");
-let o_se_group = new streetElementGroup();
+let o_se_group = new streetElementGroup({
+  center: [-84.1027104, 9.865107],
+});
 
 console.log("Set target");
 o_se_group.setTarget(document.getElementById("map_container"));
+
+// Add a single node
+o_se_group.addNode({
+  coordinate: [-84.1027104, 9.865107],
+  type: "endpoint",
+});
+
+// Add a second node (auto linked)
+o_se_group.addNode({
+  coordinate: [-84.1033104, 9.865107],
+  type: "waypoint",
+});
+
+// Add a third node (auto linked)
+o_se_group.addNode({
+  coordinate: [-84.1033104, 9.865607],
+  type: "waypoint",
+});
 
 export { o_se_group };
